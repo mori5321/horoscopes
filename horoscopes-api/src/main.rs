d99236363fd9f1f54825d6272d6855fffa43d031
mori5321 <moriwm77@gmail.com>
2021-04-26 23:44:37 +0900

    feat: set logger

--- horoscopes-api/src/main.rs
+++ horoscopes-api/src/main.rs
@@ -1,12 +1,21 @@
 use horoscopes_api::filters;
 
+use std::env;
+use warp::Filter;
+
 #[tokio::main()]
 async fn main() {
     let port: u16 = 3030;
     println!("Server is running on port: {}", port);
+    
+    if env::var_os("RUST_LOG").is_none() {
+        env::set_var("RUST_LOG", "horoscopes=debug");
+    }
+    pretty_env_logger::init();
 
-    warp::serve(filters::filters())
-        .run(([127, 0, 0, 1], port))
+    warp::serve(
+        filters::filters().with(warp::log("horoscopes"))
+    ).run(([127, 0, 0, 1], port))
         .await;
 }
 
