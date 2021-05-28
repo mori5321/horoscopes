c9f7245d1afccfd402781f378f702f92a19003b0
mori5321 <moriwm77@gmail.com>
2021-05-29 03:24:59 +0900

    feat: install linaria

--- horoscopes-client/src/index.tsx
+++ horoscopes-client/src/index.tsx
@@ -1,4 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-ReactDOM.render(<div>Hello</div>, document.getElementById("app"));
+import { css } from "@linaria/core";
+
+const App: React.VFC = () => {
+  return (
+    <div className={sampleStyle}>
+      <h1>HelloWorld</h1>
+    </div>
+  );
+};
+
+const sampleStyle = css`
+  color: red;
+`;
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- horoscopes-client/webpack.config.common.ts
+++ horoscopes-client/webpack.config.common.ts
@@ -1,6 +1,7 @@
 import * as path from "path"
 import {Configuration} from "webpack";
 import * as devServer from "webpack-dev-server";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export type WebpackConfig = Configuration & { devServer?: devServer.Configuration }
 
@@ -17,13 +18,43 @@ export const commonConfig: WebpackConfig  = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: [
+          {loader: 'babel-loader'},
+          {
+            loader: '@linaria/webpack-loader',
+            options: {
+              sourceMap: process.env.NODE_ENV !== 'production'
+            }
+          },
+          { loader: 'ts-loader' }
+        ],
       },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: process.env.NODE_ENV !== 'production'
+            }
+          }
+        ]
+      }
     ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"]
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "assets": path.resolve(__dirname, "assets"),
+    }
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    })
+  ]
 }
 
 
