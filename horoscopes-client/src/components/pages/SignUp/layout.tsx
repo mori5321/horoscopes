f04ada4a24fb6e0c07b4313082786d08e56d4a93
mori5321 <moriwm77@gmail.com>
2021-06-05 18:02:00 +0900

    a

--- horoscopes-client/src/components/pages/SignUp/layout.tsx
+++ horoscopes-client/src/components/pages/SignUp/layout.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 
 type Props = {};
-const SignUpLayout = (_props: Props) => (
-  <div>
-    <h1>Sign Up Why</h1>
-  </div>
-);
+const SignUpLayout = (_props: Props) => {
+  const name = 'tom';
+
+  return (
+    <div>
+      <h1>Sign Up, {name}</h1>
+    </div>
+  );
+};
 
 export { SignUpLayout };
