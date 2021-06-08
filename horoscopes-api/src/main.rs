e886f54e57062a6e791cd3c57729be6d4a394878
mori5321 <moriwm77@gmail.com>
2021-06-08 22:42:38 +0900

    fix: fix db pool gen method

--- horoscopes-api/src/main.rs
+++ horoscopes-api/src/main.rs
@@ -1,6 +1,6 @@
 use horoscopes_api::filters;
 use horoscopes_api::state::AppState;
-use horoscopes_api::db::gen_mysql_pool;
+use horoscopes_api::db::gen_db_pool;
 
 use std::env;
 use warp::Filter;
@@ -11,7 +11,7 @@ async fn main() {
     let port: u16 = 3030;
     println!("Server is running on port: {}", port);
 
-    let pool = Arc::new(gen_mysql_pool()); 
+    let pool = Arc::new(gen_db_pool()); 
     let app_state = Arc::new(AppState::new(pool.clone()));
 
     if env::var_os("RUST_LOG").is_none() {
